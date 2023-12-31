2cd1acc4020f07bc4933ec0e8df594940b4b7717
srikanthnani <srikanthnani1202@gmail.com>
2023-12-31 14:57:18 +0530

    updated: gemini ui :sparkles:

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { port: 3000 },
   plugins: [react()],
-})
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
