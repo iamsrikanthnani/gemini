a3d4be6868f20c6b8d7b1a5ef3f5351f32257dbd
srikanthnani <srikanthnani1202@gmail.com>
2023-12-31 14:57:18 +0530

    updated: gemini ui:sparkles:

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Text } from "@radix-ui/themes";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const isLoading = true;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex flex-col sm:flex-row h-screen bg-black p-8">
+      {/* Camera layout */}
+      <div className="lg:w-2/3 lg:h-full md:w-2/3  md:h-full sm:w-full h-4/6 sm:mr-2 mb-2 sm:mb-0">
+        <div
+          className={`p-4 justify-center flex items-center rounded-[30px] h-full ${
+            isLoading ? "border-anim" : "border-4"
+          }`}
+        >
+          <Text className="text-white">Camera Layout</Text>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      {/* Gemini response layout */}
+      <div className="lg:w-1/3 lg:h-full md:w-1/3 md:h-full sm:w-full h-2/6">
+        <div className="bg-black p-4 h-full w-full justify-center flex items-center">
+          <Text className="text-white"> Ai Layout</Text>
+          {/* FOOTER */}
+          <div className="absolute bottom-0 py-4 text-center">
+            <img
+              className="w-32 ml-2.5"
+              src={
+                "https://ppc.land/content/images/size/w1200/2023/12/Google-Gemini-AI-2.webp"
+              }
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
