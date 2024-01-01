cf54724a6e57e6807e0a3d0f282fd87f1b6f3c15
srikanthnani <srikanthnani1202@gmail.com>
2024-01-01 16:21:14 +0530

    added: vercel/analytics :chart_with_upwards_trend:

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from "./App.tsx";
 import "./global.css";
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Theme>
       <App />
     </Theme>
+    <Analytics />
   </React.StrictMode>
 );
